cf4bcfe007ebbb315c62d6527bf5f455df2c88f8
utsavsinghthc-cmd <utsavsinghthc@gmail.com>
2026-05-02 14:04:08 +0530

    Create firebase.js

---
+++ scripts/scripts/scripts/scripts/firebase.js
@@ -0,0 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
